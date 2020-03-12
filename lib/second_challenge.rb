dad690d23d3bba1ce2e908be234c5eae676d2c70
James Giosmas <giosmasj@gmail.com>
2020-03-12 04:20:44 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,18 +1,18 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
+   :montague => {
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_freinds => []
-      }, 
-    :capulet => {
+      :hero_friends => []
+   },
+   :capulet => {
       :patriarch => {},
       :matriarch => {},
-      :hero => {},
+      :heroine => {},
       :heroine_friends => []
-      }
-  }
+   }
+}
 
   
 end
